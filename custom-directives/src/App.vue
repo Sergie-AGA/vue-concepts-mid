9c1a525ec9a56d4fc137c9f190923442473f28cf
SergieAGA2 <sergiearruda@gmail.com>
2021-12-02 22:50:55 +0000

    Adds examples of local custom directives and starter files for filters and mixins

--- custom-directives/src/App.vue
+++ custom-directives/src/App.vue
@@ -4,13 +4,32 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Directives</h1>
         <p v-highlight:background="'red'">Color this</p>
+        <p v-local-highlight:background="'green'">Color this locally</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  directives: {
+    "local-highlight": {
+      bind(el, binding, vnode) {
+        var delay = 0;
+        if (binding.modifiers["delayed"]) {
+          delay = 3000;
+        }
+        setTimeout(() => {
+          if (binding.arg == "background") {
+            el.style.backgroundColor = binding.value;
+          } else {
+            el.style.color = bind.value;
+          }
+        }, delay);
+      },
+    },
+  },
+};
 </script>
 
 <style></style>
