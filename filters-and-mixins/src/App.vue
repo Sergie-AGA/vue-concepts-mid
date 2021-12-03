f2036e0d3222ed2f142c1f4317dbc53b5b58fcc0
SergieAGA2 <sergiearruda@gmail.com>
2021-12-03 20:16:17 +0000

    Furthers lesson on filters

---
+++ filters-and-mixins/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <p>{{ text }}</p>
+        <p>{{ text2 }}</p>
+        <p>{{ text | toUppercase }}</p>
+        <p>{{ text2 | toLowercase }}</p>
+        <p>{{ text | toUppercase | toLowercase }}</p>
+        <hr />
+        <input type="text" v-model="filterText" />
+        <ul>
+          <li v-for="(fruit, index) in filteredFruits" :key="index">
+            {{ fruit }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    text: "hey",
+    text2: "HEY",
+    fruits: ["Apple", "Banana", "Mango", "Melon"],
+    filterText: "",
+  }),
+
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    },
+  },
+
+  computed: {
+    filteredFruits() {
+      return this.fruits.filter((element) => {
+        return element.match(this.filterText);
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
