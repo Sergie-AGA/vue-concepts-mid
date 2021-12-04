1653cd554f6cec7b132f5d39ceca1750610a81c4
SergieAGA2 <sergiearruda@gmail.com>
2021-12-04 17:27:45 +0000

    Include assignment 18 about custom directives

---
+++ assignment-18/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1 v-myeffect:click="mymethod">Directives Exercise</h1>
+        <!-- Exercise -->
+        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    myeffect: {
+      bind(el, binding, vnode) {
+        el.addEventListener(binding.arg, binding.value);
+      },
+    },
+  },
+
+  methods: {
+    mymethod() {
+      window.alert("Hi");
+    },
+  },
+};
+</script>
+
+<style></style>
